a335332330549165e3124db921bc87d6edf0b1b7
snow1989-dev <73281344+snow1989-dev@users.noreply.github.com>
2021-10-17 10:50:07 +0300

    Add files via upload

---
+++ by/introductiontojava/topic01/section01/task02/Expression.java
@@ -0,0 +1,26 @@
+package by.introductiontojava.topic01.section01.task02;
+
+/** Вычислить знаение выражения по формуле (все переменные принимают 
+ действительное значение): (b + √(b^2+4ac))/2a)-a^3c + b^-2.
+ */
+
+public class Expression {
+
+	public static void main(String[] args) {
+
+		double a;
+		double b;
+		double c;
+		double z;
+
+		a = 6.5;
+		b = 8.7;
+		c = 16.0;
+		z = b + Math.sqrt(Math.pow(b, 2) + 4 * a * c);
+		z = z / 2 * a;
+		z = z - Math.pow(a, 3) * c + Math.pow(b, -2);
+
+		System.out.print(z);
+	}
+
+}
