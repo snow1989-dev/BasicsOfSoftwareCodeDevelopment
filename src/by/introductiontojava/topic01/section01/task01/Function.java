9d94933f70c69006760af4026347c7fe393a198f
snow1989-dev <73281344+snow1989-dev@users.noreply.github.com>
2021-10-17 11:11:27 +0300

    Add files via upload

---
+++ src/by/introductiontojava/topic01/section01/task01/Function.java
@@ -0,0 +1,23 @@
+package by.introductiontojava.topic01.section01.task01;
+
+//Найдите значение функции: z = ((a - 3)*b/2) + c
+
+public class Function {
+
+	public static void main(String[] args) {
+
+		double z;
+		double a;
+		double b;
+		double c;
+
+		a = 6;
+		b = 11;
+		c = 288;
+		z = ((a - 3) * b / 2) + c;
+
+		System.out.print(z);
+
+	}
+
+}
